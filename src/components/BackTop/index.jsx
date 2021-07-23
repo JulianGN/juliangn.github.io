b4978b237b78e6ac13a7e222d5a636bcfdfbaca3
julianjgn <contato@juliannunes.com.br>
2021-07-23 17:46:28 -0300

    Fix arrow icon

--- src/components/BackTop/index.jsx
+++ src/components/BackTop/index.jsx
@@ -4,7 +4,9 @@ import './style.css';
 export default function BackTop(){
     return(
         <div id="back-top">
-            <a href="#topo">&#8963;</a>
+            <a href="#topo">
+                <img src="imgs/up-arrow-svgrepo-com.svg" alt="Voltar ao topo" />
+            </a>
         </div>
     )
 }
